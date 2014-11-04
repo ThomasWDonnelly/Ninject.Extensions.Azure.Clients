06fab185be9a2daeb4726ca71f72e3f5dd655fb9
noocyte <jarle.nygaard@outlook.com>
2014-11-04 11:38:01 +0100

    Initial copy of code

---
+++ src/Ninject.Extensions.Azure.Clients/AzureClientsModule.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Ninject.Modules;
+
+namespace Ninject.Extensions.Azure.Clients
+{
+    public class AzureClientsModule : NinjectModule
+    {
+        private readonly string _servicebusConnection =
+            CloudConfigurationManager.GetSetting("ServiceBusConnectionString");
+
+        private readonly string _storageConnection =
+            CloudConfigurationManager.GetSetting("StorageConnectionString");
+
+        public override void Load()
+        {
+            this.BindUnlessBoundAsSingleton(c =>
+            {
+                var storageAccount = CloudStorageAccount.Parse(_storageConnection);
+                return storageAccount;
+            });
+
+            this.BindUnlessBoundAsSingleton(c =>
+            {
+                var storageAccount = Kernel.Get<CloudStorageAccount>();
+                var queueClient = storageAccount.CreateCloudQueueClient();
+                return queueClient;
+            });
+
+            this.BindUnlessBoundAsSingleton(c =>
+            {
+                var namespaceManager = NamespaceManager.CreateFromConnectionString(_servicebusConnection);
+                return namespaceManager;
+            });
+
+            this.BindUnlessBoundAsSingleton(c =>
+            {
+                var fac = MessagingFactory.CreateFromConnectionString(_servicebusConnection);
+                return fac;
+            });
+        }
+    }
+}
---
+++ src/Ninject.Extensions.Azure.Clients/ICreateClients.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace Ninject.Extensions.Azure.Clients
+{
+    public interface ICreateClients
+    {
+        Task<CloudQueue> CreateStorageQueueClientAsync(string queueName);
+        Task<QueueClient> CreateServicebusQueueClientAsync(string queueName);
+        Task<TopicClient> CreateTopicClientAsync(string topicName);
+        Task<SubscriptionClient> CreateSubscriptionClientAsync(string topicName, string subscriptionName);
+    }
+}
